3dd89b6f8a709917a0a0e65e7d895c46bc041f1b
DipsikKk <141740406+DearDany@users.noreply.github.com>
2024-01-15 19:48:19 +0200

    new tasks

--- js/8kuy_tasks.js
+++ js/8kuy_tasks.js
@@ -42,4 +42,98 @@ describe("smash", () =>  {
 
 
 
-/*-------------------------------------------------------------TASK 2---------------------------------------------------------------------*/ 
+/*-------------------------------------------------------------TASK 2---------------------------------------------------------------------*/
+
+/*
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
+ that receive a list of integers as input, 
+and return the largest and lowest number in that list, respectively.
+
+*/
+
+
+var min = function(list){
+    
+    return Math.min(...list);
+}
+
+var max = function(list){
+   
+    return Math.max(...list);
+}
+
+
+/*________________________________TEST__________________*/
+
+const Test = require('@codewars/test-compat');
+
+describe("Test", function(){
+  it("Examples", function(){
+    Test.assertEquals(min([-52, 56, 30, 29, -54, 0, -110]), -110);
+    Test.assertEquals(min([42, 54, 65, 87, 0]), 0);
+    Test.assertEquals(max([4,6,2,1,9,63,-134,566]), 566);
+    Test.assertEquals(max([5]), 5);
+  });
+});
+
+
+/*-------------------------------------------------------TASK 3------------------------------------------------------*/
+
+
+/*
+Write a function that accepts an integer n and a string s as parameters, 
+and returns a string of s repeated exactly n times.
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+/*--------------------------------------------------------------TASK 4----------------------------------------------*/
+
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+
+const check = (a, x) => {
+  return a.includes(x);
+};
+
+/*------------------------------------------------------------TASK 5 --------------------------------------------*/
+
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+*/
+
+function simpleMultiplication(number) {
+  let even = 0;
+  let odd = 0;
+    if (number % 2 === 0){
+      let even = number * 8;
+      return even;
+    }else{
+      let odd = number * 9;
+      return odd;
+    }
+}
+
+
+/*SHORT EXAMPLE*/
+
+/*
+function simpleMultiplication(n) {
+    return n * (n % 2 ? 9 : 8);
+}
+
+*/ 
+
+
+/*---------------------------------------------------------TASK 6 -----------------------------------------------*/ 
